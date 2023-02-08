725efa166c7ebdd484c1450826044be2632e521c
rafacovez <adanescollante@gmail.com>
2023-02-08 08:11:33 -0400

    coded timeToMixJuice function

--- mixed-juices.js
+++ mixed-juices.js
@@ -11,7 +11,20 @@
  * @returns {number} time in minutes
  */
 export function timeToMixJuice(name) {
-  throw new Error('Please implement the timeToMixJuice function');
+  switch (name) {
+      case "Pure Strawberry Joy":
+          return 0.5;
+      case "Energizer":
+          return 1.5;
+      case "Green Garden":
+          return 1.5;
+      case "Tropical Island":
+          return 3;
+      case "All or Nothing":
+          return 5;
+      default:
+          return 2.5;
+  }
 }
 
 /**
